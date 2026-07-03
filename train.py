87efa917e3707425222c10479313891b340cfb70
2023popn <nathanp2023@gmail.com>
2026-07-03 07:19:26 -0400

    Made trains actually follow lines

--- train.py
+++ train.py
@@ -5,18 +5,18 @@ class Train:
     def __init__(self, line):
         self.line = line
         self.passengers = []
-        self.segment_index = 0   # which pair of stations we're between
+        self.segment_index = 0   # which pair of points we're between
         self.progress = 0.0      # 0.0 to 1.0 along current segment
         self.direction = 1       # 1 forward, -1 backward along the line
 
     def update(self, dt):
-        stations = self.line.stations
-        if len(stations) < 2:
+        points = self.line.path_points
+        if len(points) < 2:
             return
 
-        a = stations[self.segment_index]
-        b = stations[self.segment_index + self.direction]
-        seg_length = ((b.x - a.x) ** 2 + (b.y - a.y) ** 2) ** 0.5
+        a = points[self.segment_index]
+        b = points[self.segment_index + self.direction]
+        seg_length = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** 0.5
         if seg_length == 0:
             return
 
@@ -25,15 +25,15 @@ class Train:
             self.progress = 0.0
             self.segment_index += self.direction
             # bounce back and forth at line ends
-            if self.segment_index <= 0 or self.segment_index >= len(stations) - 1:
+            if self.segment_index <= 0 or self.segment_index >= len(points) - 1:
                 self.direction *= -1
 
     def get_position(self):
-        stations = self.line.stations
-        a = stations[self.segment_index]
-        b = stations[self.segment_index + self.direction]
-        x = a.x + (b.x - a.x) * self.progress
-        y = a.y + (b.y - a.y) * self.progress
+        points = self.line.path_points
+        a = points[self.segment_index]
+        b = points[self.segment_index + self.direction]
+        x = a[0] + (b[0] - a[0]) * self.progress
+        y = a[1] + (b[1] - a[1]) * self.progress
         return x, y
 
     def draw(self, screen):
