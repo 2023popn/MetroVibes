4032183e4c43ca067fbec28de7cb748c1727be5a
2023popn <nathanp2023@gmail.com>
2026-07-07 09:34:59 -0400

    Some bug fixes and refactor with worldgen

---
+++ world_generation.py
@@ -0,0 +1,98 @@
+import pygame
+from settings import *
+from helpers import *
+from world import World
+from station import Station
+from line import Line
+from train import Train
+
+def generate_interchange_stations(world, num_stations):
+    for i in range(num_stations):
+        station_x = round(truncated_normal_randomization(0, SCREEN_WIDTH), -1)
+        station_y = round(truncated_normal_randomization(0, SCREEN_HEIGHT), -1)
+        station = Station(get_unique_station_name(), station_x, station_y, True)
+        world.add_interchange_station(station)
+
+def generate_line_with_minor_stations(world):
+    new_line = Line("Red", LINE_COLORS[0])
+    stations_to_add = []
+
+    total_num_interchanges = len(world.interchange_stations)
+
+    weights = []
+    while len(weights) < total_num_interchanges - 1:
+        weights = [x * 2 for x in weights]
+        weights.append(1)
+
+    num_line_interchanges = random.choices(list(range(2, total_num_interchanges+1)), weights=weights)[0]
+    line_interchanges = random.sample(world.interchange_stations, k=num_line_interchanges)
+
+    for interchange in line_interchanges:
+        stations_to_add.append(interchange)
+
+    # Generate stations between interchanges
+    for i in range(len(line_interchanges) - 1):
+        current_station = line_interchanges[i]
+        next_station = line_interchanges[i+1]
+
+        direction_vector = [next_station.x - current_station.x, next_station.y - current_station.y]
+        magnitude_noise = truncated_normal_randomization(0, 1)
+
+        x_noise = truncated_normal_randomization(-50, 50)
+        y_noise = truncated_normal_randomization(-50, 50)
+
+        minor_station_start_x = current_station.x + direction_vector[0] * magnitude_noise + x_noise
+        minor_station_start_y = current_station.y + direction_vector[1] * magnitude_noise + y_noise
+
+        generation_direction = random.choice(["x", "y", "xy"])
+        available_stations = 0
+        new_stations = []
+
+        if generation_direction == "x":
+            available_x_space = next_station.x - minor_station_start_x
+            available_stations = abs(int(available_x_space / STATION_MIN_SPACING))
+
+            num_stations = random.choice(range(available_stations + 1))
+
+            for j in range(num_stations):
+                new_station_x = round(minor_station_start_x + STATION_MIN_SPACING * j, -1)
+                new_station_y = round(minor_station_start_y, -1)
+
+                new_stations.append(Station(get_unique_station_name(), new_station_x, new_station_y))
+
+        elif generation_direction == "y":
+            available_y_space = next_station.y - minor_station_start_y
+            available_stations = abs(int(available_y_space / STATION_MIN_SPACING))
+
+            num_stations = random.choice(range(available_stations + 1))
+
+            for j in range(num_stations):
+                new_station_x = round(minor_station_start_x, -1)
+                new_station_y = round(minor_station_start_y + STATION_MIN_SPACING * j, -1)
+
+                new_stations.append(Station(get_unique_station_name(), new_station_x, new_station_y))
+
+        elif generation_direction == "xy":
+            available_x_space = next_station.x - minor_station_start_x
+            available_y_space = next_station.y - minor_station_start_y
+            available_space = min(abs(available_x_space), abs(available_y_space))
+            available_stations = abs(int(available_space / STATION_MIN_SPACING))
+
+            num_stations = random.choice(range(available_stations + 1))
+
+            for j in range(num_stations):
+                new_station_x = round(minor_station_start_x + STATION_MIN_SPACING * j * abs(available_x_space) / available_x_space, -1)
+                new_station_y = round(minor_station_start_y + STATION_MIN_SPACING * j * abs(available_y_space) / available_y_space, -1)
+
+                new_stations.append(Station(get_unique_station_name(), new_station_x, new_station_y))
+
+        current_station_index_in_line = stations_to_add.index(current_station)
+        stations_to_add[current_station_index_in_line + 1:current_station_index_in_line + 1] = new_stations
+
+    for station in stations_to_add:
+        new_line.add_station(station)
+    world.add_line(new_line)
+
+    starting_station = random.choice(new_line.stations)
+    new_train = Train(new_line)
+    world.add_train(new_train)
